e050bce8c8f9b2c4e7a6bd39a320961b7dc92c2f
Victor Liu <esternocliomastoidal@gmail.com>
2025-10-06 15:38:09 +0200

    fix #3 method FindByName implemented

---
+++ src/main/java/com/example/enfermeria/Nurse.java
@@ -0,0 +1,40 @@
+package com.example.enfermeria;
+
+public class Nurse {
+	private String user;
+	private String name;
+	private String password;
+
+	public Nurse(String user, String name, String password) {
+		this.user = user;
+		this.name = name;
+		this.password = password;
+		
+
+	}
+
+	public String getUser() {
+		return user;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
--- src/main/java/com/example/enfermeria/NurseController.java
+++ src/main/java/com/example/enfermeria/NurseController.java
@@ -1,5 +1,26 @@
 package com.example.enfermeria;
 
+import java.util.ArrayList;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class NurseController {
-	//test
+	ArrayList<Nurse> nurseList = EnfermeriaApplication.nurseList;
+
+	@GetMapping("/nurse/name")
+	public ResponseEntity<Nurse> findByName(@RequestParam(value = "name", defaultValue = "") String name) {
+
+		for (int i = 0; i < nurseList.size(); i++) {
+			if (name.equalsIgnoreCase(nurseList.get(i).getName())) {
+				return ResponseEntity.ok(nurseList.get(i));
+			}
+
+		}
+		return ResponseEntity.notFound().build();
+	}
+
 }
