e5160ff310058a6383134f9819b0168a71e183fd
Adam <askm.da6@gmail.com>
2025-10-23 17:04:55 +0200

    fix #11

--- src/main/java/com/example/enfermeria/dao/NurseRepository.java
+++ src/main/java/com/example/enfermeria/dao/NurseRepository.java
@@ -1,9 +1,13 @@
 package com.example.enfermeria.dao;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.example.enfermeria.entity.Nurse;
 
 public interface NurseRepository extends CrudRepository<Nurse, Integer> {
+
+	Optional<Nurse> findByNameIgnoreCase(String name);
 
 }
