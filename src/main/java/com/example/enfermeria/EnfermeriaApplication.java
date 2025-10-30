ce4ba7977f97a2f1886d487941401b0ed02dee52
MarcLopezMolina <marclopezmolina242006@gmail.com>
2025-10-30 15:52:50 +0100

    Code revision & CRUD initialized

--- src/main/java/com/example/enfermeria/EnfermeriaApplication.java
+++ src/main/java/com/example/enfermeria/EnfermeriaApplication.java
@@ -15,22 +15,16 @@ import java.util.ArrayList;
 public class EnfermeriaApplication {
 	public static ArrayList<Nurse> nurseList = new ArrayList<Nurse>();
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		SpringApplication.run(EnfermeriaApplication.class, args);
-
-		/*
-		nurseList.add(new Nurse(1, "Maria01", "Maria", "123", "maria@gmail.com"));
-		nurseList.add(new Nurse(2, "Irene02", "Irene", "123", "irene@gmail.com"));
-		nurseList.add(new Nurse(3, "Claudia03", "Claudia", "123", "claudia@gmail.com"));
-		nurseList.add(new Nurse(4, "Anna04", "Anna", "123", "ana@gmail.com"));
-		nurseList.add(new Nurse(5, "Cristina05", "Cristina", "123", "cristina@gmail.com"));
-		*/
 
 	}
 
 	//Method to test if Postman works
     @GetMapping("/mainTest")
-    public ResponseEntity<String> mainTest(){
+    public ResponseEntity<String> mainTest()
+    {
     	return ResponseEntity.ok("The system is working fine!");
     }
 }
